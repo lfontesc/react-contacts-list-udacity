e11ceef958c4d4b05926c11ca710035bb85b3e64
Lucas Fontes <lfonteesc@gmail.com>
2019-02-17 13:07:19 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
@@ -32,9 +34,14 @@ removeContact = (contact) => {
   render() {
     return (
       <div>
-       <ListContacts contacts={this.state.contacts}
-        onDeleteContact={this.removeContact}
-       />
+        <Route exact path='/' render={() => (
+          <ListContacts contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+          />
+        )} />
+      <Route path='/create' component={CreateContact} />
+     
+       
       </div>
     );
   }
